2e24a7a667a69c9ef91f15e946f855619a99dea2
Nilambar Sharma <nilambar@outlook.com>
2016-08-15 11:06:46 +0545

    Include search form

--- partials/no-posts-search.php
+++ partials/no-posts-search.php
@@ -1,3 +1,3 @@
 <h2><?php _e( 'Error! I can\'t find what you\'re looking for.', 'milky-way' ); ?></h2>
 <p><?php _e( 'Sorry, no posts matched your search terms. Try another search!', 'milky-way' ); ?></p>
-<?php get_template_part( 'searchform' ); ?>
+<?php get_search_form(); ?>
